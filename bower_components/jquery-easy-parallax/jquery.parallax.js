759ada9c38964c0c04cd0878080eead61ab00941
Kaspars <kasparsj@gmail.com>
2015-10-08 11:45:11 +0300

    bug fixes

--- bower_components/jquery-easy-parallax/jquery.parallax.js
+++ bower_components/jquery-easy-parallax/jquery.parallax.js
@@ -200,7 +200,7 @@
             return obj instanceof HTMLElement;
         }
         catch(e) {
-            return (typeof obj === "object") && (obj.nodeType === 1) && 
+            return (typeof obj === "object") && (obj.nodeType === 1) &&
                 (typeof obj.style === "object") && (typeof obj.ownerDocument ==="object");
         }
     }
