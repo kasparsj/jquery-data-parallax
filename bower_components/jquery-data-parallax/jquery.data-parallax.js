b4fc689d01090382ed23b42fd3b36fa9b0766857
Kaspars <kasparsj@gmail.com>
2016-10-15 15:26:22 +0300

    updated bower components

--- bower_components/jquery-data-parallax/jquery.data-parallax.js
+++ bower_components/jquery-data-parallax/jquery.data-parallax.js
@@ -18,8 +18,9 @@
     };
 
     function testTouchEvents() {
-        return 'ontouchstart' in window // works on most browsers
-            || 'onmsgesturechange' in window; // works on ie10
+        return ('ontouchstart' in window) ||
+            (navigator.maxTouchPoints > 0) ||
+            (navigator.msMaxTouchPoints > 0);
     }
 
     $.fn.parallax = function(method) {
